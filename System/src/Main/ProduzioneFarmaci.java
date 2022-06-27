b2aadf5ad0b7c9f99b3b85dae0a3f7356e7f1443
Shevraar <58468371+GabrieleZito@users.noreply.github.com>
2022-06-27 22:07:32 +0200

    iniziano le migliorie grafiche

--- System/src/Main/ProduzioneFarmaci.java
+++ System/src/Main/ProduzioneFarmaci.java
@@ -1,12 +1,27 @@
 package Main;
 
+import GestioneMagazzino.Notifica;
+
+import java.time.LocalTime;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class ProduzioneFarmaci {
     private int qtaProdotta;
-
+    private Timer timer;
     public ProduzioneFarmaci(int qtaProdotta){
         this.qtaProdotta = qtaProdotta;
+        produci();
     }
 
+    private void produci(){
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+
+            }
+        }, 60*60*1000, 60*60*1000);
+    }
 
 
 }
